eda8339c198f108ed3e69f84bfcd40542a2a0b3a
Luca D'Angelo <dangelo.luca@iisgalvanimi.edu.it>
2024-11-11 13:46:26 +0000

    parte 1 - Angular-Routing fatta

---
+++ Angular-Routing/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Routes } from '@angular/router';
+import { FruitsComponent } from './fruits/fruits.component';
+import { AnimalsComponent } from './animals/animals.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FruitsComponent,
+    AnimalsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

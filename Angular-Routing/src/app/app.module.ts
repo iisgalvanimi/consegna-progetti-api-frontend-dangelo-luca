8646448a195a54ddc897a6742fe4b53a5291cce6
Luca D'Angelo <dangelo.luca@iisgalvanimi.edu.it>
2024-11-12 12:52:26 +0000

    fatto basic request Chiamate HTTP

--- Angular-Routing/src/app/app.module.ts
+++ Angular-Routing/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; //Importa Il modulo http
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +9,7 @@ import { Routes } from '@angular/router';
 import { FruitsComponent } from './fruits/fruits.component';
 import { AnimalsComponent } from './animals/animals.component';
 import { GenericoComponent } from './generico/generico.component';
+import { FooComponent } from './foo/foo.component';
 
 
 
@@ -15,11 +18,13 @@ import { GenericoComponent } from './generico/generico.component';
     AppComponent,
     FruitsComponent,
     AnimalsComponent,
-    GenericoComponent
+    GenericoComponent,
+    FooComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

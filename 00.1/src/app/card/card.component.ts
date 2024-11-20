b50f87dab6a3cb671d664e25b99bdfd2b9e4aca9
Luca D'Angelo <dangelo.luca@iisgalvanimi.edu.it>
2024-11-20 12:50:06 +0000

    sistemato errore card.ts e completato card.html

--- 00.1/src/app/card/card.component.ts
+++ 00.1/src/app/card/card.component.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -13,22 +12,29 @@ export class CardComponent implements OnInit {
   datiCarta: any;
   caricamento = false;
 
- constructor(private route: ActivatedRoute, private http: HttpClient){}
-  
- ngOnInit(): void {
-    this.route.paramMap.subscribe(params =>{const NomeCarta = params.get('name')!; this.fetchdatiCarta(NomeCarta)})
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const NomeCarta = params.get('name')!;
+      this.fetchdatiCarta(NomeCarta);
+    });
   }
 
-  fetchdatiCarta(NomeCarta: string):void{
+  fetchdatiCarta(NomeCarta: string): void {
     this.caricamento = true;
-    const url = 'https://api.scryfall.com/cards/named?fuzzy=${NomeCarta}';
-    this.http.get(url).subscribe((data) => {this.datiCarta = data; this.caricamento = false},
-    (error) =>{
-      console.error('carta non trovata',error);
-      this.caricamento = false;
-
-    }
-  )
+    const url = `https://api.scryfall.com/cards/named?fuzzy=${NomeCarta}`; // Usare backticks
+    this.http.get(url).subscribe(
+      (data) => {
+        this.datiCarta = data;
+        this.caricamento = false;
+      },
+      (error) => {
+        console.error('Carta non trovata', error);
+        this.caricamento = false;
+      }
+    );
   }
 
 }
+
